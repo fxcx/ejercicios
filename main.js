b9f623783682b2167c80290fefa0974a5ee95ca5
fxcx <87233669+fxcx@users.noreply.github.com>
2023-04-11 16:54:02 -0300

    subir ejercicio

---
+++ main.js
@@ -0,0 +1,27 @@
+// Defino las variables 
+const lowerLimitEven = 2;
+const upperLimitEven = 10;
+const lowerLimitOdd = 1;
+const upperLimitOdd = 9;
+
+// los números pares de lowerLimitEven y upperLimitEven
+for(let num = lowerLimitEven; num <= upperLimitEven; num++) {
+  if(i % 2 === 0) {
+    console.log(`El ${num} es par`);
+  }
+}
+
+// los números impares de lowerLimitOdd y upperLimitOdd
+for(let num = lowerLimitOdd; num <= upperLimitOdd; num++) {
+  if(i % 2 !== 0) {
+    console.log(`El ${num} es impar`);
+  }
+}
+
+//ejercicio 2
+
+/*      
+
+estoy tratando de hacerlo en otra rama para despues fuccionarlo
+
+*/ 
