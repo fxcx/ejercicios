5e078e761cf05ac10c7e410eef1d56cf54c6edc6
fxcx <87233669+fxcx@users.noreply.github.com>
2023-05-18 02:23:30 -0300

    correccion

--- calse21/dom.js
+++ calse21/dom.js
@@ -1,27 +1,32 @@
 // capturar valor de los imputs
-const inputsTexto = document.querySelectorAll('input[type="text"]');
-const password = document.querySelectorAll('input[tipe="password"]');
-const check = document.querySelector('input[name="newsletter"]');
+window.addEventListener('DOMContentLoaded', () => {
+  
+ const inputsTexto = document.querySelectorAll('input[type="text"]');
+ const Password = document.querySelectorAll('input[type="password"]');
+ const check = document.querySelector('input[name="newsletter"]');
+ const form = document.querySelector('form')
 
 
-
-
-// foreach se activa cada vez que ingresa un numero
-inputsTexto.forEach (input => {
+ // foreach se activa cada vez que ingresa un numero
+ inputsTexto.forEach (input => {
   input.addEventListener('input', event => {
     const valor = event.target.value;
     event.target.value = valor.replace(/[0-9]/, ''); // remplaza cualquier numero por una cadena vacia y lo elimina
   });
-});
+ });
 
-
-
-// checkbox
-
-
-
-check.addEventListener('change',(event) => {
+ //password
+  Password = (password,confirmPassword) => password === confirmPassword
+ //chekbox
+  check.addEventListener('change',(event) => {
     console.log(event.target.checked); // checked para que dvuelva un booleano // usar para checkbox
    
  });
 
+ form.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+
+ }) 
+
+})
