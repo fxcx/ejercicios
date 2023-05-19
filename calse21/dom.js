49943342d0594dc5ae0ff2d0234f9e4b547bef1c
fxcx <87233669+fxcx@users.noreply.github.com>
2023-05-19 16:46:12 -0300

    practica de dom

--- calse21/dom.js
+++ calse21/dom.js
@@ -1,32 +1,93 @@
-// capturar valor de los imputs
-window.addEventListener('DOMContentLoaded', () => {
-  
- const inputsTexto = document.querySelectorAll('input[type="text"]');
- const Password = document.querySelectorAll('input[type="password"]');
- const check = document.querySelector('input[name="newsletter"]');
- const form = document.querySelector('form')
+// html
+window.addEventListener("DOMContentLoaded", () => {
+  // capturar valor de los imputs
+  const firstNameInput = document.querySelector('input[name="firstname"]');
+  const lastNameInput = document.querySelector('input[name="lastname"]');
+  const usernameInput = document.querySelector('input[name="username"]');
+  const passwordInput = document.querySelector('input[name="password"]');
+  const confirmPasswordInput = document.querySelector(
+    'input[name="confirmpassword"]'
+  );
+  const birthdayInput = document.querySelector('input[name="birthday"]');
+  const termsAndConditionsInput = document.querySelector(
+    'input[name="termsconditions"]'
+  );
+  const form = document.querySelector("form");
 
-
- // foreach se activa cada vez que ingresa un numero
- inputsTexto.forEach (input => {
-  input.addEventListener('input', event => {
-    const valor = event.target.value;
-    event.target.value = valor.replace(/[0-9]/, ''); // remplaza cualquier numero por una cadena vacia y lo elimina
-  });
- });
-
- //password
-  Password = (password,confirmPassword) => password === confirmPassword
- //chekbox
-  check.addEventListener('change',(event) => {
-    console.log(event.target.checked); // checked para que dvuelva un booleano // usar para checkbox
+  //funciones
+  const validateName = (name) => {
+    const regExp = new RegExp(/^[a-zA-Z\s]+$/);
+    const isCorrect = name.length > 0 && regExp.test(name); // numero de cadena es mayor a 0 y tenga la exprecion regexp
    
- });
+    return isCorrect; // me devuelve un boolean
+  };
 
- form.addEventListener('submit', (event) => {
-  event.preventDefault()
+  const validateUserName = (username) => {
+    const regExp = new RegExp(/^[a-zA-Z0-9]+$/);
+    return username.length > 0 && regExp.test(username);
+  };
+
+  const validatePassword = (password, confirmPassword) =>
+    password === confirmPassword;
+
+  const validateBirthday = (birthday) => {
+    const today = new Date();
+    const eighteenYearsAgo = new Date(
+      today.getFullYear() - 18,
+      today.getMonth(),
+      today.getDate()
+    );
+    const birthdayDate = new Date(birthday);
+    return birthdayDate <= eighteenYearsAgo;
+  };
+
+  // combierto la cadena de texto guardada devuelta a un objeto
+  const user = JSON.parse(localStorage.getItem("user"));
+  console.log(user);
+
+  // repasar es lo mismo que lo de arriba
+  // JSON.parse(user)
+  // const userJson = JSON.parse(user)
+  // asi puedo trabajarlo con un objeto
+  // console.log(userJson.password);
 
 
- }) 
+ // evento - evento del surmulario - submit
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-})
+    const firstName = firstNameInput.value; //creo una variable al que le asigno el valor de la captura del imput firstnameimput
+    const lastName = lastNameInput.value;
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+    const confirmPassword = confirmPasswordInput.value;
+    const birthday = birthdayInput.value;
+    const termsAndConditions = termsAndConditionsInput.checked;
+
+    let isValid = true; // la condicion
+    // isValid = isValid && validateName(firstName)
+    isValid &&= validateName(firstName); //validateName es el nombre de la funcion y (el parametro es variable firstname de la linea 59)
+    isValid &&= validateName(lastName);
+    isValid &&= validateUserName(username);
+    isValid &&= validatePassword(password, confirmPassword);
+    isValid &&= validateBirthday(birthday);
+
+    if (!isValid) {
+      alert("Invalid input");
+      return;
+    }
+
+    const newUser = {
+      firstName,
+      lastName,
+      password,
+      confirmPassword,
+      username,
+      birthday,
+      termsAndConditions,
+    };
+     
+    // guardo el usuario con json.stringify que guarda string
+    localStorage.setItem("user", JSON.stringify(newUser));
+  });
+});
