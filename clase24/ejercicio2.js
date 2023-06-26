ba003511e0be6a99cdb317f659e367baf37cd735
fxcx <87233669+fxcx@users.noreply.github.com>
2023-06-26 15:40:52 -0300

    EJERCICIO 2

--- clase24/ejercicio2.js
+++ clase24/ejercicio2.js
@@ -1,11 +1,11 @@
-const TIMER_ONE_SECOND = 1000
+const ONE_SECOND_IN_MS = 1000
 
 const timer = () => {
   for (var i = 1; i <= 10; i++) {
     (function (num) {
       setTimeout(() => {
         console.log(num);
-      }, num * TIMER_ONE_SECOND);
+      }, num * ONE_SECOND_IN_MS);
     })(i);
   }
 }
