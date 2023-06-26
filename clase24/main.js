25b8afefb360f8d938a681ef0d861ccd7d761fec
fxcx <87233669+fxcx@users.noreply.github.com>
2023-06-26 14:56:58 -0300

    ejecicio1__clase24

--- clase24/main.js
+++ clase24/main.js
@@ -15,6 +15,37 @@
 
 // la segunda forma de importarlo
 
-import utils from './utils.js'
+// import utils from './utils.js'
 
-console.log(utils.multiply(20, 5))
+// console.log(utils.multiply(20, 5))
+
+
+// fay sistem
+
+// import fs from 'node:fs'
+
+// const data = fs.readFileSync('./datos.txt', 'utf-8')
+
+// console.log(data)
+
+
+import fs from 'node:fs';
+
+// Leer el archivo datos.txt
+const data = fs.readFileSync('./datos.txt', 'utf-8');
+
+// Contar la cantidad de números pares
+const numbers = data.split(/\D+/);
+let count = 0;
+
+for (const num of numbers) {
+  if (num !== '' && parseInt(num) % 2 === 0) {
+    count++;
+  }
+}
+
+// Crear y escribir en el archivo de respuesta
+fs.writeFileSync('./datos.txt', `La cantidad de números pares en el archivo datos.txt es: ${count}`);
+
+console.log('Archivo respuesta.txt creado y actualizado con la respuesta.')
+
